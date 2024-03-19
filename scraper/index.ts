80ffdd8b42575de129b34ba96b3c2159eeb5a2b5
Jaycen <saitakusei@gmail.com>
2024-03-19 17:38:46 +0900

    Fix lint issue

--- scraper/index.ts
+++ scraper/index.ts
@@ -1,14 +1,15 @@
-import logger from "./lib/logger";
-import crawler from "./cron/crawler";
-import cron from "node-cron";
+import logger from './lib/logger'
+import crawler from './cron/crawler'
+import cron from 'node-cron'
 
-const schedule = process.env.CRON_SCHEDULE || "* */1 * * * *";
-logger.info(`Cron job is scheduled to run at ${schedule}`);
+const schedule = process.env.CRON_SCHEDULE || '* */1 * * * *'
+
+logger.info(`Cron job is scheduled to run at ${schedule}`)
 
 cron.schedule(schedule, async () => {
-  logger.info("Scraping Suumo to get the latest items...")
+  logger.info('Scraping Suumo to get the latest items...')
 
   await crawler()
 
-  logger.info("Suumo items have been scraped and saved to the database.")
-});
+  logger.info('Suumo items have been scraped and saved to the database.')
+})
